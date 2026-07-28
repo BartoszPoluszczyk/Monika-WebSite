de3f7e52a58188657b4bfccf36521c965b11e902
Bartosz Poluszczyk <bartosz12360@gmail.com>
2026-07-28 11:30:39 +0200

    !st HTML

--- dietetyk_site/main/views.py
+++ dietetyk_site/main/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse("Witaj na stronie Moniki!")
+    return render(request, "main/home.html")
