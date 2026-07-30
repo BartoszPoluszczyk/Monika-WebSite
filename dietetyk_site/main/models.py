91e516a2de9ac6435f90f77b70215ca7df229ec5
Bartosz Poluszczyk <bartosz12360@gmail.com>
2026-07-30 11:20:08 +0200

    AdminPanel

--- dietetyk_site/main/models.py
+++ dietetyk_site/main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Specialization(models.Model):
+    name = models.CharField(
+        max_length=150,
+        verbose_name="Nazwa specjalizacji"
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Specjalizacja"
+        verbose_name_plural = "Specjalizacje"
