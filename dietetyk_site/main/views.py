22e18790b1dab208a73255a8d5b14296ac9af3f9
Bartosz Poluszczyk <bartosz12360@gmail.com>
2026-07-30 11:14:23 +0200

    Update - about, css, photo

--- dietetyk_site/main/views.py
+++ dietetyk_site/main/views.py
@@ -3,3 +3,22 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "main/home.html")
+
+
+def about(request):
+    context = {
+        "title": "O mnie",
+        "description": """
+            Jestem Monika, dyplomowany dietetyk. Jestem bardzo słodka, choć nie spożywam cukru.
+            Tobie, drogi Czytelniku, również tego odradzam, jeśli nie chcesz mieć paskudnych zmian skórnych na plecach.
+            Bartosz dość późno sięgnął po rozum do głowy, toteż jego plecy idealnie jeszcze się nie prezentują.
+            Jeśli i Ty chcesz mieć piękne i gładkie plecy, skontaktuj się ze mną.
+        """,
+        "specializations": [
+            "Suplementacja",
+            "Redukcja masy ciała",
+            "Dieta dobierana do indywidualnych potrzeb",
+        ],
+    }
+
+    return render(request, "main/about.html", context)
